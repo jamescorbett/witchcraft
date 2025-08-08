1c28346ee481c4bf571d9f478663a30073537975
Jacob Gorm Hansen <jhansen@dropbox.com>
2025-08-08 19:47:21 +0200

    add timings to nfcorpus-score.sh

--- nfcorpus-score.sh
+++ nfcorpus-score.sh
@@ -1,5 +1,6 @@
 rm -rf output.txt
 
-cargo run --release --features accelerate querycsv $HOME/src/xtr-warp/beir/nfcorpus/questions.test.tsv output.txt &&\
+cargo build --release --features accelerate
+time cargo run --release --features accelerate querycsv $HOME/src/xtr-warp/beir/nfcorpus/questions.test.tsv output.txt &&\
 
 python score.py output.txt $HOME/src/xtr-warp/beir/nfcorpus/collection_map.json $HOME/src/xtr-warp/beir/nfcorpus/qrels.test.json
